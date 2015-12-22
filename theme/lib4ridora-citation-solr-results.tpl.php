e191a2edc0e38e5eb28491099f51b05397601fa8
MorganDawe <mdawe@discoverygarden.ca>
2015-12-22 10:35:30 -0400

    Added a div wrapper for styling purposes.

--- theme/lib4ridora-citation-solr-results.tpl.php
+++ theme/lib4ridora-citation-solr-results.tpl.php
@@ -15,6 +15,7 @@
 <div id="lib4ridora-citation-solr-results">
   <?php foreach ($citations as $citation): ?>
   <div class="lib4ri-citation-solr-results-citation">
+    <div class="lib4ridora-citation-solr-results-inner">
     <?php print $citation['citation']; ?>
     <div class="bib-detail-record">
       <div><?php $cit_pid = $citation['pid']; print l(t("Detailed Record"), "/islandora/object/$cit_pid"); ?></div>
@@ -22,6 +23,7 @@
     <div class="bib-versions">
       <?php print $citation['pdf_listing']; ?>
     </div>
+      </div>
   </div>
   <?php endforeach; ?>
 </div>
